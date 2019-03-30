964c8495b742c692c599fc62191112983ecd0d45
Jared Grove <jaredkgrove@gmail.com>
2019-03-30 22:18:06 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,23 @@
+class Author 
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def posts 
+    @posts
+  end
+  
+  def add_post(post)
+    post.author = self
+    self.posts << post
+  end
+  
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    post.author = self
+    self.posts << post
+  end
+  
+end
